b00f72f368167cb699813022718b6e08d4d6c3bb
Christine Cunniff <ccunniff@tonicdesign.com>
2016-08-11 09:33:18 -0400

    Added tags to the buttons. Added button sound file and set the path.

--- RetroCalulator/RetroCalulator/ViewController.swift
+++ RetroCalulator/RetroCalulator/ViewController.swift
@@ -7,17 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundURL = NSURL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundURL)
+        } catch let error as NSError {
+            print(error.debugDescription)
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func numberPressed(btn: UIButton!) {
+    
     }
 
 
