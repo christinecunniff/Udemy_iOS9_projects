dcb2792f6883e34ecde8bef241441df5348e2830
Christine Cunniff <ccunniff@tonicdesign.com>
2016-08-12 11:14:58 -0400

    Created outlets and actions for UI.

--- rpg-oop/rpg-oop/ViewController.swift
+++ rpg-oop/rpg-oop/ViewController.swift
@@ -2,7 +2,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var printLabel: UILabel!
+    @IBOutlet weak var playerHpLabel: UILabel!
+    @IBOutlet weak var enemyHpLabel: UILabel!
+    @IBOutlet weak var enemyImage: UIImageView!
+    @IBOutlet weak var chestButton: UIButton!
+    
+    @IBAction func onChestTapped(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
