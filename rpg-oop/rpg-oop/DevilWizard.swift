dc0aa78d534d3d06e93cf1f6facbc79c92253c35
Christine Cunniff <ccunniff@tonicdesign.com>
2016-08-12 10:11:46 -0400

    Created DarkWizard class.

---
+++ rpg-oop/rpg-oop/DevilWizard.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+class DevilWizard: Enemy {
+    
+    override var loot: [String] {
+        return ["Magic Wand", "Dark Amulet", "Salted Pork"]
+    }
+    
+    override var type: String {
+        return "Devil Wizard"
+    }
+}
