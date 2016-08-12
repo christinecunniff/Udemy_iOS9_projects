ddf7fd1f4f5a7a9bf41ef049a4a4b9e12a725b3e
Christine Cunniff <ccunniff@tonicdesign.com>
2016-08-12 13:52:18 -0400

    Initial commit Section4 Lecture 51.

---
+++ Section4_Lecture51_OOP/Section4_Lecture51_OOP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Section4_Lecture51_OOP
+//
+//  Created by Christine Cunniff on 8/12/16.
+//  Copyright © 2016 TonicDesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
