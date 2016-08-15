771b3ba737c4396a218051d8fce7801d4fffd99c
Christine Cunniff <ccunniff@tonicdesign.com>
2016-08-15 09:51:32 -0400

    Added details to player attack and set starting label.

--- Section4_Lecture51_OOP/Section4_Lecture51_OOP/ViewController.swift
+++ Section4_Lecture51_OOP/Section4_Lecture51_OOP/ViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var winnerLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
     
     var playerOne: PlayerOne!
     var playerTwo: PlayerTwo!
@@ -11,17 +12,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        playerOne = PlayerOne(startingHp: 100, attackPower: 10)
+        playerTwo = PlayerTwo(startingHp: 120, attackPower: 10)
         
     }
     
     @IBAction func onPlayerOneAttack(sender: AnyObject) {
-    
+        if playerTwo.attack((playerOne.attackPower)) {
+            NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: #selector(ViewController.onPlayerTwoAttack(_:)), userInfo: nil, repeats: false)
+        }
+        
+        if !playerTwo.isAlive {
+            winnerLabel.text = "Player 2 defeated!!!"
+        }
     }
     
     @IBAction func onPlayerTwoAttack(sender: AnyObject) {
-    
+        if playerOne.attack((playerTwo.attackPower)) {
+            NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: #selector(ViewController.onPlayerOneAttack(_:)), userInfo: nil, repeats: false)
+        }
+        
+        if !playerOne.isAlive {
+            winnerLabel.text = "Player 1 defeated!!!"
+        }
     }
 
-
+    @IBAction func onPlayAgainTapped(sender: AnyObject) {
+        winnerLabel.text = ""
+        playAgainButton.hidden = true
+    }
+    
+    
+   
 }
-
