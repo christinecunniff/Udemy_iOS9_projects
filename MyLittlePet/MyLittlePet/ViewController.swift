70d1ecb2240e436f8693e24e449029ca186fc6a0
Christine Cunniff <ccunniff@tonicdesign.com>
2016-08-15 15:35:51 -0400

    Set up rudimentary UI layout.

--- MyLittlePet/MyLittlePet/ViewController.swift
+++ MyLittlePet/MyLittlePet/ViewController.swift
@@ -2,9 +2,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monsterImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        var imageArray = [UIImage]()
+//        for var x = 1; x <= 4; x++ {
+//            let img = UIImage(named: "idle\(x).png")
+//            imageArray.append(img!)
+//        }
+//        
+//        monsterImage.animationImages = imageArray
+//        monsterImage.animationDuration = 0.8
+//        monsterImage.animationRepeatCount = 0
+//        monsterImage.stopAnimating()
     }
     
 }
