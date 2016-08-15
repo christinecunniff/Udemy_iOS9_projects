683d11f9bfcfb395a86bb429748a9828ccde99e9
Christine Cunniff <ccunniff@tonicdesign.com>
2016-08-15 16:14:36 -0400

    Set up monsterImage animation.

--- MyLittlePet/MyLittlePet/ViewController.swift
+++ MyLittlePet/MyLittlePet/ViewController.swift
@@ -7,16 +7,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        var imageArray = [UIImage]()
-//        for var x = 1; x <= 4; x++ {
-//            let img = UIImage(named: "idle\(x).png")
-//            imageArray.append(img!)
-//        }
-//        
-//        monsterImage.animationImages = imageArray
-//        monsterImage.animationDuration = 0.8
-//        monsterImage.animationRepeatCount = 0
-//        monsterImage.stopAnimating()
+        var imageArray = [UIImage]()
+        for image in imageArray {
+            let img = UIImage(named: "idle\(image).png")
+            imageArray.append(img!)
+        }
+        
+        monsterImage.animationImages = imageArray
+        monsterImage.animationDuration = 0.8
+        monsterImage.animationRepeatCount = 0
+        monsterImage.stopAnimating()
     }
     
 }
