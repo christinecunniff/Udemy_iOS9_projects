96270b43961a9d09bbb0895fffd77359f3c43bd6
Christine Cunniff <ccunniff@tonicdesign.com>
2016-08-15 17:06:10 -0400

    Enabled touches for food and heart images.

--- MyLittlePet/MyLittlePet/DragImage.swift
+++ MyLittlePet/MyLittlePet/DragImage.swift
@@ -5,6 +5,7 @@ class DragImage: UIImageView {
     
     var originalPosition: CGPoint!
     
+    // called BEFORE views are loaded, so the center is not gauranteed
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -14,14 +15,17 @@ class DragImage: UIImageView {
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
+        originalPosition = self.center
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        <#code#>
+        if let touch = touches.first {
+            let position = touch.locationInView(self.superview)
+            self.center = CGPointMake(position.x, position.y)
+        }
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        <#code#>
+        self.center = originalPosition
     }
 }
