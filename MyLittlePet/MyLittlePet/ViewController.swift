3d31a5e272a46a63a35d096722a59186efad0f00
Christine Cunniff <ccunniff@tonicdesign.com>
2016-08-17 13:49:40 -0400

    Added logic to send a notification when an item is dropped on the monster traget.

--- MyLittlePet/MyLittlePet/ViewController.swift
+++ MyLittlePet/MyLittlePet/ViewController.swift
@@ -3,10 +3,19 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var monsterImage: MonsterImg!
-    @IBOutlet weak var foodImage: UIImageView!
-    @IBOutlet weak var heartImage: UIImageView!
+    @IBOutlet weak var foodImage: DragImage!
+    @IBOutlet weak var heartImage: DragImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        foodImage.dropTarget = monsterImage
+        heartImage.dropTarget = monsterImage
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(itemDroppedOnCharacter), name: "onTargetDropped", object: nil)
+    
+    }
+    
+    func itemDroppedOnCharacter(notif: AnyObject) {
+        print("ITEM DROPPED ON CHARACTER")
     }
 }
