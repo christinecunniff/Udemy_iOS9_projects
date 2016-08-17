2513de6a6196bfe2aab0c38cbf0a4fe68ce7436f
Christine Cunniff <ccunniff@tonicdesign.com>
2016-08-17 13:23:05 -0400

    Created animation for monster death.

--- MyLittlePet/MyLittlePet/MonsterImg.swift
+++ MyLittlePet/MyLittlePet/MonsterImg.swift
@@ -8,9 +8,14 @@ class MonsterImg: UIImageView {
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        playIdleAnimation()
     }
     
     func playIdleAnimation() {
+        
+        self.image = UIImage(named: "idle1.png")
+        self.animationImages = nil
+        
         var imgArray = [UIImage]()
         for var x = 1; x <= 4; x += 1 {
             let img = UIImage(named: "idle\(x).png")
@@ -20,6 +25,23 @@ class MonsterImg: UIImageView {
         self.animationImages = imgArray
         self.animationDuration = 0.8
         self.animationRepeatCount = 0
+        self.startAnimating()
+    }
+    
+    func playDeathAnimation() {
+        
+        self.image = UIImage(named: "dead5.png")
+        self.animationImages = nil
+        
+        var imgArray = [UIImage]()
+        for var x = 1; x <= 5; x += 1 {
+            let img = UIImage(named: "dead\(x).png")
+            imgArray.append(img!)
+        }
+        
+        self.animationImages = imgArray
+        self.animationDuration = 0.8
+        self.animationRepeatCount = 1
         self.startAnimating()
     }
 }
--- MyLittlePet/MyLittlePet/ViewController.swift
+++ MyLittlePet/MyLittlePet/ViewController.swift
@@ -2,15 +2,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var monsterImage: UIImageView!
+    @IBOutlet weak var monsterImage: MonsterImg!
     @IBOutlet weak var foodImage: UIImageView!
     @IBOutlet weak var heartImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
-    
-    
+    }
 }
-
