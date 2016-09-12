f3f82697228746cc0c6605ccfa178134251c46cd
Christine Cunniff <ccunniff@tonicdesign.com>
2016-09-12 13:10:57 -0400

    Set up back button to dismiss active view

---
+++ Fav_Place_App/Fav_Place_App/DetailVC.swift
@@ -0,0 +1,28 @@
+//
+//  DetailVC.swift
+//  Fav_Place_App
+//
+//  Created by Christine Cunniff on 9/12/16.
+//  Copyright © 2016 TonicDesign. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
